f524c285547622e9bada9576218681400293c6af
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-04-18 10:53:45 -0500

    Se incluyo la biblioteca stdio.h, ademas de definir las variables a usar

--- Ejercicio12.c
+++ Ejercicio12.c
@@ -1,1 +1,8 @@
 //Ejercicio 12: Leer un número entero de 3 dígitos y determinar si tiene digito 1
+//Incluimos la biblioteca stdio.h para leer y mostrar datos
+#include <stdio.h>
+int main (){
+    //Definir las variables
+    int n=0, digito1=0, digito2=0, digito3=0;
+    return 0;
+}
