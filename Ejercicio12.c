a479415cbee03f17acbd639173d6dc496cfcd5d3
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-04-18 10:42:52 -0500

    Pedir al usuario el numero, se añadió return y otro comentario

---
+++ Ejercicio12.c
@@ -0,0 +1,13 @@
+//Ejercicio 12: Leer un número entero de 3 dígitos y determinar si tiene digito 1
+//Usamos la biblioteca stdio.h para mostrar y leer datos al usuario
+#include <stdio.h>
+int main (){
+    //definir las variables a usar
+    int i, n=0, digito1=0, digito2=0, digito3=0;
+    //pedir al usuario el numero
+    printf("Por favor ingrese un numero entero de 3 digitos: ");
+    scanf("%d",&n);
+    //Ver cuales son los digitos del numero
+    
+    return 0;
+}
