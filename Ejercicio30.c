662a577db327c259e5eec80d97d605287488821f
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-04-18 18:36:56 -0500

    Usar la biblioteca stdio.h

--- Ejercicio30.c
+++ Ejercicio30.c
@@ -1,1 +1,6 @@
 //Ejercicio 30: Leer un número entero y mostrar todos su componentes numericos, osea, a quienes sea multiplo
+//añadir la biblioteca stdio.h para mostrar y leer datos
+#include <stdio.h>
+int main (){
+    
+}
