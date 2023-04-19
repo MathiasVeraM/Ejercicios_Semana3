1f3138adedadda7e8c9f55cdd9f7ac0a3de068df
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-04-18 19:02:33 -0500

    Utilizar la biblioteca stdio.h

--- Ejercicio39.c
+++ Ejercicio39.c
@@ -1,3 +1,5 @@
 /*Se define la serie de Fibonacci como la serie que comienza con los dígitos 1 y 0 y va sumando progresivamente los dos últimos elementos de la serie, así:
  0 1 1 2 3 5 8 13 21 34.......
 Utilizando el concepto de ciclo, generar la serie de Fibonacci hasta llegar o sobrepasar el número 10000.*/
+//Utilizar la biblioteca stdio.h
+#include <stdio.h>
