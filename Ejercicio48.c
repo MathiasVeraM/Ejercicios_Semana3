7aa7d37ae0de44905e826ad264ca6aed7734efa0
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-04-18 23:35:05 -0500

    Me olvidé de return, igual funcionaba sin el pero para mantener la estructura

--- Ejercicio48.c
+++ Ejercicio48.c
@@ -24,4 +24,5 @@ int main (){
         k++; //Aumenta k para que no sea bucle infinito, es solo un contador
     }
     //Se comprobo el correcto funcionamiento del programa crea las series requeridas
+    return 0;
 }
