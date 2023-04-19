88dc1f126b266586a815762edebcafb18aedca94
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-04-18 20:05:25 -0500

    Me olvide de solicitar los datos al usuario, asi que lo añadí en este cambio

--- Ejercicio21.c
+++ Ejercicio21.c
@@ -4,6 +4,9 @@
 int main (){
     //Definir las variables a usar 
     int num=0, digito=0, suma=0;
+    //Solicitar al usuario un numero entero
+    printf("Por favor, ingrese un numero entero para sumar sus digitos: ");
+    scanf("%d",&num);
     //Escribir la estructura de la repeticion
     while (num!=0) {
         digito = num % 10; //La variable digito sera el resultado del mod del numero para 10
