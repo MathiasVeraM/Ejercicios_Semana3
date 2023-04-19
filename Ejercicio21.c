23299d7338b628c75d52cd77efa8bd94b5ab9d0c
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-04-18 19:44:09 -0500

    Definir las variables a usar, numero ingrresado por usuario, variable digito que cambia, la suma de digitos, que los va a almacenar

--- Ejercicio21.c
+++ Ejercicio21.c
@@ -1,3 +1,8 @@
 //Ejercicio 21: Leer un número entero y determinar a cuánto es igual la suma de sus dígitos.
 //Incluir la biblioteca stdio.h, leer e imprimir datos del usuario
 #include <stdio.h>
+int main (){
+    //Definir las variables a usar 
+    int num=0, digito=0, suma=0;
+    
+}
