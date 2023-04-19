9e0cedc322f1f926921bb4d7dec892a9d3fcd132
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-04-18 22:12:45 -0500

    Esta comprobado el funcionamiento y es igual a la primera version de ciclos anidados

--- Ejercicio48V2.c
+++ Ejercicio48V2.c
@@ -1,2 +1,15 @@
 //Mismo objetivo, me parece que se puede simplificar la solucion bastante, sin usar ciclos anidados
 //Por tanto esta es una version opcional del ejercicio48 sin ciclos anidados
+#include <stdio.h>
+int main (){
+    //Definir las variables
+    int i=0, j=1, acumulador=0;
+    //Hacer la repeticion
+    for(i=0;i<=9;i++){
+        printf("%d\t %d\n", i, j);
+        acumulador++;
+        if(acumulador%2==0){
+            j++;
+        }
+    }
+}
