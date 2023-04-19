e4981e62ea092e7718fc8198a35c718f8bfca288
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-04-18 19:17:12 -0500

    Imprimir los 3 primeros numeros de la repeticion, los primeros dos antes de la repeticion, para que no vayan imprimiendose consecutivamente y arruinen la serie

--- Ejercicio39.c
+++ Ejercicio39.c
@@ -6,9 +6,12 @@ Utilizando el concepto de ciclo, generar la serie de Fibonacci hasta llegar o so
 int main (){
     //Definir las variables a usar
     int num1=0, num2=0, n=0;
+    //Imprimir los dos primeros numeros de la serie
+    printf ("%d %d",num1,num2);
     //Escribir la estructura de la repeticion a usar
     while (n<=10000){
         n = num1+num2; //n es la suma de los dos numeros anteriores
-        
+        //Imprimir el tercer numero
+        printf(" %d",n);
     }
 }
